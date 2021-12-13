4b745f8b9b342fed619f04fc6b67ed8bda70780b
Christine Contreras <christineec23@yahoo.com>
2021-12-13 14:39:23 -0600

    make goals and questions dependent on bookclub books

--- app/models/bookclub_book.rb
+++ app/models/bookclub_book.rb
@@ -1,8 +1,8 @@
 class BookclubBook < ApplicationRecord
   belongs_to :bookclub
   belongs_to :book
-  has_many :goals
-  has_many :guide_questions
+  has_many :goals, dependent: :destroy
+  has_many :guide_questions, dependent: :destroy
 
   validates :book_id, {presence: true }
   validates :bookclub_id, {presence: true }
